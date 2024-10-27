ea7bb06b99a5b4cdb721391f26efd77ba1aa5df7
EldritchGoose <kskhshs738@gmail.com>
2024-10-27 18:07:11 +0200

    dev-2

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, Flask!"
+def resume():
+    title = "My Resume"
+    return render_template('resume.html', title=title)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
